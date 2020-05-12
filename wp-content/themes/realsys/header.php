ee8a54afc567c41a0ef2babb3897873b36cb2f3f
Mike <m.j.prochazka@gmail.com>
2020-05-12 11:56:32 +0200

    analytics

--- wp-content/themes/realsys/header.php
+++ wp-content/themes/realsys/header.php
@@ -5,6 +5,14 @@
     <meta name="google-signin-client_id" content="<?php echo GOOGLE_ID; ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-164036485-2"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-164036485-2');
+	</script>	
 	<?php wp_head(); ?>
 </head>
 
