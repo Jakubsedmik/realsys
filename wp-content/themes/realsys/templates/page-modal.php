ebe6a9483cdbb17f11230d1fe7f52a2a210443fe
Mike <m.j.prochazka@gmail.com>
2020-05-15 22:13:28 +0200

    page modal

---
+++ wp-content/themes/realsys/templates/page-modal.php
@@ -0,0 +1,98 @@
+<section>
+	<div class="slider">
+		<div class="wrapper" style="display:flex;flex-direction:row;">
+
+
+
+			<!-- Modal -->
+			<div class="kontk-maj-modal" id="kontmajModal" tabindex="-1" role="dialog" aria-labelledby="kontmajModalLabel" aria-hidden="true">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content light-blue-bg rounded-b shadow-big text-center">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div class="modal-body">
+							<h2 class="sz-tit"><?php echo _e("Kontaktovať majiteľa", "realsys"); ?></h2>
+							<img class="maj-img mb-sm-4 mb-3" src="<?php echo FRONTEND_IMAGES_PATH; ?>/avatar.png" alt="">
+							<h2 class="sz-tit">Filip Šeršík</h2>
+							<p class="mb-sm-5 mb-3">Cena kontaktu je <strong>1 kredit</strong> Chcete kontaktovať majiteľa?</p>
+							<a href="#" class="btn mb-sm-5 mb-3"><?php echo _e("Získať kontakt", "realsys"); ?></a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+			<!-- Modal -->
+			<div class="kontk-maj-kredit-modal" id="kontmajkreditModal" tabindex="-1" role="dialog" aria-labelledby="kontmajkreditModalLabel" aria-hidden="true">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content light-blue-bg rounded-b shadow-big text-center">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div class="modal-body">
+							<h2 class="sz-tit"><?php echo _e("Kontaktovať majiteľa", "realsys"); ?></h2>
+							<img class="maj-img mb-sm-4 mb-3" src="<?php echo FRONTEND_IMAGES_PATH; ?>/avatar.png" alt="">
+							<h2 class="sz-tit">Filip Šeršík</h2>
+							<p class="mb-2 p-w-kredit">Cena kontaktu je <strong>1 kredit</strong> Chcete kontaktovať majiteľa?</p>
+							<p class="mb-4"><strong>Na účte nemáte dostatok kreditov</strong></p>
+							<a href="#" class="btn mb-sm-5 mb-3"><?php echo _e("Získať kontakt", "realsys"); ?></a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+			<!-- Modal -->
+			<div class="transakce-modal" id="transakceModal" tabindex="-1" role="dialog" aria-labelledby="transakceModalLabel" aria-hidden="true">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content light-blue-bg rounded-b shadow-big text-center">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div class="modal-body">
+							<img class="maj-img mb-sm-4 mb-3" src="<?php echo FRONTEND_IMAGES_PATH; ?>/ikony/sign-check.png" alt="">
+							<h2 class="sz-tit"><?php echo _e("Transakcia bola úspešná!", "realsys"); ?></h2>
+
+							<p class=""><?php echo _e("Na Váš účet sme pripísali", "realsys"); ?></p>
+							<h3 class="kredity-big">182</h2>
+								<p class="mb-sm-5 mb-3"><?php echo _e("Kreditov", "realsys"); ?></p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+			<!-- Modal -->
+			<div class="kontk-maj-info-modal" id="kontmajinfoModal" tabindex="-1" role="dialog" aria-labelledby="kontmajinfoModalLabel" aria-hidden="true">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content light-blue-bg rounded-b shadow-big text-center">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div class="modal-body">
+							<h2 class="sz-tit"><?php echo _e("Kontaktovať majiteľa", "realsys"); ?></h2>
+							<img class="maj-img mb-3" src="<?php echo FRONTEND_IMAGES_PATH; ?>/avatar.png" alt="">
+							<h2 class="sz-tit mb-2">Filip Šeršík</h2>
+							<p class="mb-2">filip.sersik@outlook.com</p>
+							<p class="mb-sm-5 mb-3">+420 720 968 217</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+
+
+
+		</div>
+	</div>
+</section>
