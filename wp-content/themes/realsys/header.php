3b6bb107c3fcf619da437494c4090aff43cd0314
Mike <m.j.prochazka@gmail.com>
2020-09-22 18:32:59 +0200

    úprava analytics kodu

--- wp-content/themes/realsys/header.php
+++ wp-content/themes/realsys/header.php
@@ -17,6 +17,16 @@
         gtag('config', 'UA-164036485-2');
     </script>
 
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-164036485-3"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-164036485-3');
+		</script>		
+
 
     <?php Pixel::initiatePixel(); ?>
 
